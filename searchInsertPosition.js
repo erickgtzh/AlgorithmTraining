670db25d84a525da090243c0194b195ac2efbf6a
Erick Gutiérrez <0193662@up.edu.mx>
2022-01-31 20:31:27 -0600

    Add files via upload

---
+++ searchInsertPosition.js
@@ -0,0 +1,27 @@
+// /**
+//  * @param {number[]} nums
+//  * @param {number} target
+//  * @return {number}
+//  */
+var searchInsert = function(nums, target) {
+    let low = 0;
+    let high = nums.length - 1;
+    while(low < high) { 
+        let mid = Math.floor((high + low) / 2);
+        if(nums[mid] === target) return mid;
+        else if(nums[mid] > target) {
+            high = mid - 1;
+        }
+        else if(nums[mid] < target) {
+            low = mid + 1;
+        } else {
+          return mid;
+        }
+    }
+    return low + 1;
+};
+
+let nums = [1,3,5,6];
+let target = 7;
+
+searchInsert(nums, target)
